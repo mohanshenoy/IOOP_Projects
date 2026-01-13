261ebd13730aa9457056c7f14d8259376557f2cc
Mohan Shenoy <mohanshenoy@gmail.com>
2026-01-13 23:16:09 +0530

    updated L2ppt

---
+++ L2ppt/src/LeftShift.java
@@ -0,0 +1,9 @@
+
+public class LeftShift{ 
+	public static void main(String[] args) {
+		byte a = 8, i;
+		//i = a <<2;​
+		System.out.println("value of a is : "+a);
+		System.out.println("value of i is : "+i );
+	}
+}
---
+++ L2ppt/src/RightShift.java
@@ -0,0 +1,14 @@
+
+public class RightShift {
+
+	public static void main(String[] args) {
+		int a = 32;
+		a = a >>2;
+		System.out.println("value of a is: "+a);
+
+		a = 35;
+		a = a >>2;
+		System.out.println("value of a is: "+a);
+	}
+
+}
