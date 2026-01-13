b97cb63a80f63d2e159f110e275f87544dcba6cb
Mohan Shenoy <mohanshenoy@gmail.com>
2026-01-13 23:17:09 +0530

    fixed compilation errors

--- L2ppt/src/LeftShift.java
+++ L2ppt/src/LeftShift.java
@@ -2,7 +2,7 @@
 public class LeftShift{ 
 	public static void main(String[] args) {
 		byte a = 8, i;
-		//i = a <<2;​
+		i = (byte) (a <<2);​
 		System.out.println("value of a is : "+a);
 		System.out.println("value of i is : "+i );
 	}
