73bc5d9cf470b016ac50322107c2808753b79fae
Mohan Shenoy <mohanshenoy@gmail.com>
2026-01-14 06:18:51 +0530

    added more classes related to order precedence , arrays, 2d arrays

---
+++ L2ppt/src/ArrayProgram2.java
@@ -0,0 +1,10 @@
+
+public class ArrayProgram2 {
+	public static void main(String[] args) {
+	 int a[] = new int[10];
+	 a[0]=10;
+	 a[1]=15;
+	 System.out.println(a[1]);
+	 System.out.println(a.length);
+	}
+}
